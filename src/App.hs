fe2574552fcc54a382460a20d538302043a095ae
shokohara <shokohara@users.noreply.github.com>
2017-06-07 20:03:09 +0900

    .

--- src/App.hs
+++ src/App.hs
@@ -1,27 +1,21 @@
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module App where
 
 import GHC.Generics (Generic)
-import Data.Text hiding (minimum)
-import Network.Google.Resource.Compute.Instances.Insert
-import Network.Google.Compute.Types
-import Control.Lens                 ((&), (.~), (<&>), (?~))
-import qualified Network.Google         as Google
-import Network.Google
-import qualified Network.Google.Compute         as Compute
-import Network.Google.Compute
-import Network.Google.Compute.Types
+import Control.Lens ((&), (.~), (<&>), (?~))
 import System.IO (stdout)
+import System.Directory
+import System.Process
+import Data.String.Utils
 
 run :: IO ()
 run = do
-  lgr  <- Google.newLogger Google.Debug stdout
-  env  <- Google.newEnv <&> (Google.envLogger .~ lgr) . (Google.envScopes .~ Compute.computeScope)
-  runResourceT . Google.runGoogle env $ do
-    send $ instancesInsert "shokohara-157622" "asia-northeast1-a" (instance' & insnName ?~ "instance-1" & insnMachineType ?~ "zones/asia-northeast1-a/machineTypes/n1-standard-1" & insnNetworkInterfaces .~ [networkInterface & niName ?~ "global/networks/default"] & insnDisks .~ [attachedDisk & adBoot ?~ True & adInitializeParams ?~ (attachedDiskInitializeParams & adipSourceImage ?~ "projects/debian-cloud/global/images/family/debian-8")])
-    return ()
-  runResourceT . Google.runGoogle env $ do
-    send $ instancesDelete "shokohara-157622" "asia-northeast1-a" ""
+  putStrLn "start"
+  cwd <- getCurrentDirectory
+  directories <- listDirectory cwd
+  mapM_ putStrLn $ directories
+--  pwd <- strip <$> readCreateProcess (shell "pwd") ""
+--  readCreateProcess (shell "ls") "" >>= putStrLn
+  mapM (\pwd -> readCreateProcess (shell $ "git -C " ++ pwd ++ " status") "" >>= putStrLn) directories
   return ()
